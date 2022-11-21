e210407ef7efb07cf7327522245ad2502b7cc74d
Sebastian Lazzeri <sebastiano.lazzeri@gmail.com>
2022-11-21 11:22:58 +0100

    fatto

--- giocatori.js
+++ giocatori.js
@@ -1,6 +1,6 @@
-var val_2 = Math.floor((Math.random() * 100) + 1);
-var val_1 = Math.floor((Math.random() * 100) + 1);
-var posta = Math.floor((Math.random() * 100) + 1);
+var val_2 = Math.floor((Math.random() * 99) + 1);
+var val_1 = Math.floor((Math.random() * 99) + 1);
+var posta = Math.floor((Math.random() * 99) + 1);
 console.log('Numero generato:', posta);
 console.log('Giocatore1:', val_1);
 console.log('Giocatore2:', val_2);
--- giocatori.ts
+++ giocatori.ts
@@ -1,6 +1,7 @@
-let val_2:number= Math.floor((Math.random()*100)+1);
-let val_1:number= Math.floor((Math.random()*100)+1);
-let posta:number=Math.floor((Math.random()*100)+1);
+let val_2:number= Math.floor((Math.random()*99)+1);
+let val_1:number= Math.floor((Math.random()*99)+1);
+let posta:number= Math.floor((Math.random()*99)+1);
+
 console.log('Numero generato:',posta);
 console.log('Giocatore1:',val_1);
 console.log('Giocatore2:',val_2);
